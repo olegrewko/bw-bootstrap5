702f9d9e9c866d8f997ad6fc15d868e0846dba37
olegrewko <olegrewko@gmail.com>
2024-10-25 16:11:49 +0300

    01

---
+++ docs/js/main.js
@@ -0,0 +1,19 @@
+// $(".slider-blog__inner").slick({
+//   arrows: false,
+//   prevArrow:
+//     '<button type="button" class="slick-prev"><img src="img/arrow-left.svg" alt="img/arrow-left.svg" /></button>',
+//   nextArrow:
+//     '<button type="button" class="slick-next"><img src="img/arrow-right.svg" alt="img/arrow-right.svg" /></button>',
+//   dots: true,
+//   autoplay: true,
+//   autoplaySpeed: 3000,
+//   fade: true,
+//   responsive: [
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         arrows: false,
+//       },
+//     },
+//   ],
+// });
