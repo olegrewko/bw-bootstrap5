73a68e3042e572ef5c59981684702b8acb6740b2
olegrewko <olegrewko@gmail.com>
2024-10-25 17:56:33 +0300

    meddle

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,19 +1,102 @@
-// $(".slider-blog__inner").slick({
-//   arrows: false,
-//   prevArrow:
-//     '<button type="button" class="slick-prev"><img src="img/arrow-left.svg" alt="img/arrow-left.svg" /></button>',
-//   nextArrow:
-//     '<button type="button" class="slick-next"><img src="img/arrow-right.svg" alt="img/arrow-right.svg" /></button>',
-//   dots: true,
-//   autoplay: true,
-//   autoplaySpeed: 3000,
-//   fade: true,
-//   responsive: [
-//     {
-//       breakpoint: 768,
-//       settings: {
-//         arrows: false,
-//       },
-//     },
-//   ],
-// });
+(() => {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
+
+window.addEventListener("scroll", function () {
+  document
+    .getElementById("header-nav")
+    .classList.toggle("headernav-scroll", window.scrollY > 135);
+});
+
+const offcanvasCartEl = document.getElementById("offcanvasCart");
+const offcanvasCart = new bootstrap.Offcanvas(offcanvasCartEl);
+
+document.getElementById("cart-open").addEventListener("click", (e) => {
+  e.preventDefault();
+  offcanvasCart.toggle();
+});
+
+document.querySelectorAll(".closecart").forEach((item) => {
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+    offcanvasCart.hide();
+    let href = item.dataset.href;
+    document.getElementById(href).scrollIntoView();
+  });
+});
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $("#top").fadeIn();
+    } else {
+      $("#top").fadeOut();
+    }
+  });
+
+  $("#top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 500);
+    return false;
+  });
+
+  $(".owl-carousel-full").owlCarousel({
+    margin: 20,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      500: {
+        items: 2,
+      },
+      700: {
+        items: 3,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  });
+});
+$(".radio input").on("click", function (e) {
+  console.clear();
+  console.log(this.checked); // всегда true;
+  e.preventDefault();
+
+  setTimeout(() => $(this).prop("checked", !this.checked).trigger("change"));
+});
+$(".menu a, .totop").on("click", function (event) {
+  //отменяем стандартную обработку нажатия по ссылке
+  event.preventDefault();
+
+  //забираем идентификатор бока с атрибута href
+  var id = $(this).attr("href"),
+    //узнаем высоту от начала страницы до блока на который ссылается якорь
+    top = $(id).offset().top;
+
+  //анимируем переход на расстояние - top за 1500 мс
+  $("body,html").animate(
+    {
+      scrollTop: top,
+    },
+    1500
+  );
+});
